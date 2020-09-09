8ce3aed3a5c1384d8202f6e725f9d782604b49a6
wuwenwen <wenwenwenwu@foxmail.com>
2020-09-09 16:45:28 +0800

    banner初具规模

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div class="home-content">
+      <my-swiper :swiperImgs="swiperImgs" :height="swiperHeight"></my-swiper>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import MySwiper from "@c/swiper/MySwiper.vue";
+export default {
+  components: {
+    MySwiper,
+  },
+  data: function () {
+    return {
+      swiperImgs: [
+        require("@imgs/swiper-1.jpg"),
+        require("@imgs/swiper-2.jpg"),
+        require("@imgs/swiper-3.jpg"),
+        require("@imgs/swiper-4.jpg"),
+        require("@imgs/swiper-5.jpg"),
+        require("@imgs/swiper-6.jpg"),
+        require("@imgs/swiper-7.jpg"),
+        require("@imgs/swiper-8.jpg"),
+      ],
+      swiperHeight: "184px",
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style lang="stylus" scoped>
 .home {
   width: 100%;
   height: 100%;
-  font-size: 32px;
+
+  &-content {
+    height: 100%;
+  }
 }
 </style>
--- src/components/swiper/MySwiper.vue
+++ src/components/swiper/MySwiper.vue
@@ -1,13 +1,50 @@
 <template>
-    
+  <swiper :swiperOption="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="(item, index) in swiperImgs" :key="index">
+      <img class="swiper-slide-img" :src="item" alt srcset :style="{ height: height }" />
+    </swiper-slide>
+    <!-- Optional controls -->
+    <template v-slot:pagination>
+      <div class="swiper-pagination"></div>
+    </template>
+  </swiper>
 </template>
 
 <script>
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 export default {
-    
-}
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  directives: {
+    swiper: directive
+  },
+  props: {
+    height: {
+      type: String,
+      default: "auto",
+    },
+    swiperImgs: {
+      type: Array,
+      required: true,
+      default: function () {
+        return [];
+      },
+    },
+  },
+  data: function () {
+    return {
+      // swiper 配置
+      swiperOption: {
+        // 自动切换
+        autoplay: true,
+        // 自动高度，让 siwper 的高度跟随 slide 的高度变化
+        autoheight: true,
+      },
+    };
+  },
+};
 </script>
-
-<style lang="scss" scoped>
-
-</style>
