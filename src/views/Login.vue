16589518c3f2aa0ae3efed596320a7aa49866ab6
wuwenwen <wenwenwenwu@foxmail.com>
2020-11-16 10:21:45 +0800

    登陆注册肉体完成

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,15 @@
 <template>
 <div class="login-page">
-    <navigation-bar :pageName="登陆" @onLeftClick="onBackClick"></navigation-bar>
+    <navigation-bar :pageName="'登陆'" @on-left-click="onBackClick"></navigation-bar>
     <div class="login-page-content">
-
+        <div class="input-container">
+            <input v-model="username" type="text" placeholder="用户名" />
+        </div>
+        <div class="input-container">
+            <input v-model="password" type="text" placeholder="密码" />
+        </div>
+        <div class="login-page-content-login page-commit" @click="onLoginClick">登陆</div>
+        <a class="login-page-content-register" @click="onToRegisterClick">注册新用户</a>
     </div>
 </div>
 </template>
@@ -16,13 +23,61 @@ export default {
         NavigationBar,
     },
 
+    data: function () {
+        return {
+            username: "",
+            password: "",
+        };
+    },
+
     methods: {
         onBackClick: function () {
             this.$router.go(-1);
         },
+
+        onLoginClick: function () {
+            if (this.username.length === 0 || this.password.length === 0) {
+                alert("用户名或密码未输入");
+                return;
+            }
+        },
+
+        onToRegisterClick: function () {
+            this.$router.push({
+                name: "register",
+                params: {
+                    routerType: "push"
+                }
+            })
+        }
     },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "@css/style.scss";
+
+.login-page {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: white;
+
+    &-content {
+        width: 100%;
+        height: 100%;
+
+        &-login {
+            margin-top: 40%;
+        }
+
+        &-register {
+            font-size: $infoSize;
+            color: $hintColor;
+            margin-top: $marginSize;
+            padding: $marginSize;
+            float: right;
+        }
+    }
+}
 </style>
---
+++ src/views/Register.vue
@@ -0,0 +1,75 @@
+<template>
+<div class="register-page">
+    <navigation-bar :pageName="'注册'" @on-left-click="onBackClick"></navigation-bar>
+    <div class="register-page-content">
+        <div class="input-container">
+            <input v-model="username" type="text" placeholder="用户名" />
+        </div>
+        <div class="input-container">
+            <input v-model="password" type="text" placeholder="密码" />
+        </div>
+        <div class="input-container">
+            <input v-model="confirmPassword" type="text" placeholder="密码" />
+        </div>
+        <div class="register-page-content-register page-commit" @click="onRegisterClick">注册</div>
+    </div>
+</div>
+</template>
+
+<script>
+import NavigationBar from "@c/currency/NavigationBar.vue";
+export default {
+    name: "register",
+
+    components: {
+        NavigationBar,
+    },
+
+    data: function () {
+        return {
+            username: "",
+            password: "",
+            confirmPassword: ""
+        }
+    },
+
+    methods: {
+        onBackClick: function () {
+            this.$router.go(-1);
+        },
+
+        onRegisterClick: function () {
+            if (this.username.length === 0) {
+                alert("请完善用户名")
+                return
+            }
+            if (this.password.length === 0 || this.password != this.confirmPassword) {
+                alert("请完善密码")
+                return
+            }
+            //与原生交互，保存用户名和密码
+        }
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@css/style.scss";
+
+.register-page {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: white;
+
+    &-content {
+        width: 100%;
+        height: 100%;
+
+        &-register {
+            margin-top: 40%;
+        }
+
+    }
+}
+</style>
